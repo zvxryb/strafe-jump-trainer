3ecbd89b98c4b192b5d03dc099653410f743ea12
Mike Lodato <zvxryb@gmail.com>
2019-10-08 15:38:07 -0400

    Package script: take version number as an argument

--- package.py
+++ package.py
@@ -1,7 +1,11 @@
-import os
+import os, re, sys
 from zipfile import ZipFile, ZIP_DEFLATED
 
-DST_PATH = 'strafe-jump-trainer-{}.zip'.format(os.environ['TRAVIS_TAG'])
+if len(sys.argv) != 2 or not re.fullmatch(r'\d+\.\d+\.\d+', sys.argv[1]):
+    print("usage: package.py <version_number>")
+    sys.exit(1)
+
+DST_PATH = 'strafe-jump-trainer-{}.zip'.format(sys.argv[1])
 with ZipFile(DST_PATH, mode='x', compression=ZIP_DEFLATED) as z:
     z.write('./LICENSE')
     z.write('./README.md')
